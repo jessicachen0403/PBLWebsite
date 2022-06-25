e2569a436a432410eb95e5ca1f2b8d157639a64d
Jessica <jessicachen0403@gmail.com>
2022-06-25 01:53:33 -0500

    db

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -4,14 +4,14 @@ import {useState} from "react";
 
 const ItemDetails = () => {
   const { id } = useParams();
-  // const { data: item, error, isPending } = useFetch('http://localhost:8000/items/' + id);
-  const { data: item, error, isPending } = useFetch('https://my-json-server.typicode.com/jessicachen0403/PBLWebsite2022/items/' + id);
+  const { data: item, error, isPending } = useFetch('http://localhost:8000/items/' + id);
+  // const { data: item, error, isPending } = useFetch('https://my-json-server.typicode.com/jessicachen0403/PBLWebsite2022/items/' + id);
   // const history = useHistory();
   const [purchased, setPurchased] = useState(false);
 
   const handleClick = () => {
-    // fetch('http://localhost:8000/items/' + item.id, {
-    fetch('https://my-json-server.typicode.com/jessicachen0403/PBLWebsite2022/items/' + item.id, {
+    fetch('http://localhost:8000/items/' + item.id, {
+    // fetch('https://my-json-server.typicode.com/jessicachen0403/PBLWebsite2022/items/' + item.id, {
       method: 'DELETE'
     }).then(() => {
       // history.push('/');
